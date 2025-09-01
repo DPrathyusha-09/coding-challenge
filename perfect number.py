99328ca39641e8564f1dc9849153aa29f6518f7c
DPrathyusha-09 <dprathyushareddy09@gmail.com>
2025-09-01 15:50:28 +0000

    day3

---
+++ perfect number.py
@@ -0,0 +1,9 @@
+n=int(input())
+divisors=0
+for i in range(1,n):
+    if n%i==0:
+        divisors+=i
+if divisors==n:
+    print(f"{n} is a perfect number")
+else:
+    print(f"{n} is not a perfect number")
